4c8cb63c43f7742615cb162a02664f35e00848c0
Max <max@Maxs-Mac-mini.local>
2015-04-26 20:31:48 +0300

    Move question_id_of_answer from CommentsHelper to Comment's question_id

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,12 @@ class Comment < ActiveRecord::Base
   validates :body, presence: true
   validates :commentable, presence: true
   validates :commentable_type, inclusion: ['Question', 'Answer']
+
+  def question_id
+    if commentable_type == 'Question'
+      commentable_id
+    elsif commentable_type == 'Answer'
+      commentable.question_id
+    end
+  end
 end
