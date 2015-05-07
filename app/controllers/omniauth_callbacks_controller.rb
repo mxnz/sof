e4531557ca4335cee0bebb9408882086d8b2591d
Max <max@Maxs-Mac-mini.local>
2015-05-07 17:07:24 +0300

    Replace session[:identity_id] by session['devise.identity_id']

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -15,7 +15,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: auth.provider.capitalize) if is_navigational_format?
       else
-        session[:identity_id] = @user.identities.first.id
+        session['devise.identity_id'] = @user.identities.first.id
         redirect_to new_user_registration_url
       end
     end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def create
-    identity_id = session[:identity_id]
+    identity_id = session['devise.identity_id']
     identity = identity_id.present? ? Identity.find(identity_id) : nil
     if identity.present?
       user = User.find_by(email: sign_up_params[:email])
       if user.present?
         identity.update!(confirm_code: SecureRandom.hex, email: user.email)
-        session[:identity_id] = nil
         IdentityMailer.confirm_email(user, identity).deliver_now
         redirect_to identity_path(identity)
         return
@@ -16,15 +15,12 @@ class RegistrationsController < Devise::RegistrationsController
 
     super
 
-    if identity.present? && resource.persisted?
-      identity.update!(user: resource)
-      session[:identity_id] = nil
-    end
+    identity.update!(user: resource) if identity.present? && resource.persisted?
   end
   
   protected
     def build_resource(hash=nil)
       super
-      self.resource.without_password = true if session[:identity_id].present?
+      self.resource.without_password = true if session['devise.identity_id'].present?
     end
 end
