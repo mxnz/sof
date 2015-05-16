9a7566e59b5687d013f39da160ff6682c5734f32
Max <max@Maxs-Mac-mini.local>
2015-05-17 01:47:30 +0300

    Add method expectations specs for testing PrivatePub invoking

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -24,6 +24,11 @@ RSpec.describe AnswersController, type: :controller do
         create_answer
         expect(question.answers.last.attachments.count).to eq 2
       end
+
+      it "broadcasts to question readers" do
+        expect(PrivatePub).to receive(:publish_to).with("/questions/#{question.id}", anything)
+        create_answer
+      end
     end
 
     context 'with invalid data' do
@@ -38,6 +43,11 @@ RSpec.describe AnswersController, type: :controller do
 
       it "doesn't save new answer" do
         expect { create_invalid_answer }.to_not change(Answer, :count)
+      end
+
+      it "broadcasts nothing" do
+        expect(PrivatePub).to_not receive(:publish_to)
+        create_invalid_answer
       end
     end
   end
@@ -66,6 +76,11 @@ RSpec.describe AnswersController, type: :controller do
         it "saves new attachments" do
           expect { update_answer }.to change { answer.attachments.count }.by(2)
         end
+
+        it 'broadcasts to question readers' do
+          expect(PrivatePub).to receive(:publish_to).with("/questions/#{answer.question_id}", anything)
+          update_answer
+        end
       end
 
       context 'with invalid data' do
@@ -74,15 +89,25 @@ RSpec.describe AnswersController, type: :controller do
         it "doesn't update answer" do
           expect { update_invalid_answer }.to_not change { answer.reload.body }
         end
+
+        it 'broadcasts nothing' do
+          expect(PrivatePub).to_not receive(:publish_to)
+          update_invalid_answer
+        end
       end
     end
 
     context "another user's answer" do
       let(:another_answer) { create(:answer) }
+      let(:update_another_answer) { patch :update, id: another_answer, answer: upd_answer.attributes, format: :json }
 
       it "doesn't update answer" do
-        expect { patch :update, id: another_answer, answer: upd_answer.attributes, format: :json }.
-          to_not change { another_answer.reload.body }
+        expect { update_another_answer }.to_not change { another_answer.reload.body }
+      end
+
+      it 'broadcasts nothing' do
+        expect(PrivatePub).to_not receive(:publish_to)
+        update_another_answer
       end
     end
     
@@ -90,19 +115,29 @@ RSpec.describe AnswersController, type: :controller do
       context "by question's author" do
         let!(:question) { create(:question, user: current_user) }
         let!(:answer) { create(:answer, question: question) }
+        let(:update_best_by_author) { patch :update_best, id: answer, answer: { best: true }, format: :json }
 
         it "updates the best flag for that answer" do
-          expect { patch :update_best, id: answer, answer: { best: true }, format: :json }.
-            to change { answer.reload.best }.from(false).to(true)
+          expect { update_best_by_author }.to change { answer.reload.best }.from(false).to(true)
+        end
+
+        it 'broadcasts to question readers' do
+          expect(PrivatePub).to receive(:publish_to).with("/questions/#{question.id}", anything)
+          update_best_by_author
         end
       end
       
       context "by user, who is not a question's author" do
         let(:another_answer) { create(:answer) }
+        let(:update_best_by_not_author) { patch :update_best, id: another_answer, answer: { best: true }, format: :json }
         
         it "doesn't update best flag for that answer" do
-          expect { patch :update_best, id: another_answer, answer: { best: true }, format: :json }.
-            to_not change { another_answer.reload.best }
+          expect { update_best_by_not_author }.to_not change { another_answer.reload.best }
+        end
+
+        it 'broadcasts nothing' do
+          expect(PrivatePub).to_not receive(:publish_to)
+          update_best_by_not_author
         end
       end
     end
@@ -122,6 +157,11 @@ RSpec.describe AnswersController, type: :controller do
       it 'removes one' do
         expect { delete_own_answer }.to change(Answer, :count).by(-1)
       end
+
+      it 'broadcasts to question readers' do
+        expect(PrivatePub).to receive(:publish_to).with("/questions/#{answer.question_id}", anything)
+        delete_own_answer
+      end
     end
 
     context "an another user's answer" do
@@ -130,6 +170,11 @@ RSpec.describe AnswersController, type: :controller do
 
       it "doesn't remove one" do
         expect { delete_anothers_answer }.to_not change(Answer, :count)
+      end
+
+      it "broadcasts nothing" do
+        expect(PrivatePub).to_not receive(:publish_to)
+        delete_anothers_answer
       end
     end
   end
