be075f160b67d040ef4308bc0348e62d02cc8d96
Max <max@Maxs-Mac-mini.local>
2015-05-24 01:25:00 +0300

    Remove error comment from questions_digest_job.rb

--- app/jobs/questions_digest_job.rb
+++ app/jobs/questions_digest_job.rb
@@ -3,7 +3,7 @@ class QuestionsDigestJob < ActiveJob::Base
 
   def perform
     today = Time.now.utc.to_date
-    body = SubsMailer.questions_digest_body(today) #questions_digest_body(today)
+    body = SubsMailer.questions_digest_body(today)
     return if body.blank?
     email_subs = EmailSub.where(question_id: nil).where("coalesce(sent_at, '1970-01-01') < :today", today: today).find_each do |email_sub|
       SubsMailer.questions_digest_email(email_sub.id, body).deliver_later
