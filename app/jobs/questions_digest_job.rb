ac3cd16e43f2a8334035504108c9396173cab6db
Max <max@Maxs-Mac-mini.local>
2015-05-25 01:48:00 +0300

    Add EmailSub.to_questions_digest_not_sent_after scope

--- app/jobs/questions_digest_job.rb
+++ app/jobs/questions_digest_job.rb
@@ -5,7 +5,7 @@ class QuestionsDigestJob < ActiveJob::Base
     today = Time.now.utc.to_date
     body = SubsMailer.questions_digest_body(today)
     return if body.blank?
-    email_subs = EmailSub.where(question_id: nil).where("coalesce(sent_at, '1970-01-01') < :today", today: today).find_each do |email_sub|
+    email_subs = EmailSub.to_questions_digest_not_sent_after(today).find_each do |email_sub|
       SubsMailer.questions_digest_email(email_sub.id, body).deliver_later
     end
   end
--- app/models/email_sub.rb
+++ app/models/email_sub.rb
@@ -4,4 +4,8 @@ class EmailSub < ActiveRecord::Base
 
   validates :user, presence: true
   validates :question_id, uniqueness: { scope: :user_id }
+
+  scope :to_questions_digest_not_sent_after, ->(date) { where(question_id: nil).
+    where("coalesce(sent_at, '1970-01-01') < :date", date: date)
+  }
 end
