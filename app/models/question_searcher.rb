32b9e3939a83d898e755f8fe4114baac430b149e
Max <max@Maxs-Mac-mini.local>
2015-05-26 15:12:11 +0300

    Refactor QuestionSearcher

--- app/models/question_searcher.rb
+++ app/models/question_searcher.rb
@@ -1,36 +1,27 @@
 class QuestionSearcher
-  DOMAINS = {
-    'everywhere' => :everywhere,
-    'in questions' => :questions,
-    'in answers' => :answers,
-    "in questions' comments" => :question_comments,
-    "in answers' comments" => :answer_comments
-  }
+  DOMAINS = [
+    ['everywhere', :everywhere],
+    ['in questions', :questions],
+    ['in answers', :answers],
+    ["in questions' comments", :question_comments],
+    ["in answers' comments", :answer_comments]
+  ]
 
-  def self.search(str, domain)
-    query = Riddle::Query.escape(str)
-    condition = DOMAINS[domain]
-    case condition
-    when :everywhere
-      search_everywhere(query)
-    when :questions
-      search_in_questions(query)
+  def initialize(str, domain)
+    @query = Riddle::Query.escape(str)
+    @condition = (domain == 'everywhere' || domain == 'questions') ? nil : domain
+    @index = (domain == 'everywhere') ? 'full_questions_index_core' : nil
+  end
+
+  def search
+    if @condition.present?
+      Question.search(conditions: { @condition => @query })
     else
-      search_in_other_domain(query, condition)
+      Question.search(@query, index: @index)
     end
   end
 
-  private
-    
-    def self.search_everywhere(query)
-      Question.search(query, index: 'full_questions_index_core')
-    end
-
-    def self.search_in_questions(query)
-      Question.search(query)
-    end
-
-    def self.search_in_other_domain(query, condition)
-      Question.search(index: 'full_questions_index_core', conditions: { condition => query })
-    end
+  def self.search(str, domain)
+    self.new(str, domain).search
+  end
 end
